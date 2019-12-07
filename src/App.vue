d2d01e1c26fecd20db29787d5ae9fdc4de304c75
Matt Melquiond <matt.LLVW@gmail.com>
2019-12-07 10:48:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <h1 class="title"><span class="pink">$</span> E.XEC.SH</h1>
+    <Terminal></Terminal>
+  </div>
+</template>
+
+<script>
+import Terminal from "./components/Terminal";
+
+export default {
+  name: 'app',
+  components: {
+    Terminal,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+  html {
+    background-color: black;
+  }
+
+  .title {
+    color: white;
+    font-size: 49px;
+  }
+
+  .pink {
+    color: deeppink;
+  }
+</style>
