8103f290aeda407eaa44fa7455c5dc2c2fb86430
Matt Melquiond <matt.LLVW@gmail.com>
2020-12-03 08:51:18 +0100

    functional

--- models/gifs.go
+++ models/gifs.go
@@ -12,80 +12,31 @@ import (
 	"runtime"
 )
 
-// TODO: create custom gif struct and associated methods
 // a rendered image extracted from a gif with its timing
 type RenderedImg struct {
 	Output string
 	Delay  int
 }
 
-func GetFromUrl(url string) (g *gif.GIF, err error) {
+type AnsiGif struct {
+	Gif      *gif.GIF
+	Rendered []RenderedImg
+}
+
+func (g *AnsiGif) Get(url string) (err error) {
 	res, err := http.Get(url)
 	if err != nil {
-		return g, err
+		return err
 	}
 	defer res.Body.Close()
-	g, err = gif.DecodeAll(res.Body)
+	g.Gif, err = gif.DecodeAll(res.Body)
 	if err != nil {
-		return g, err
+		return err
 	}
-	return
+	return nil
 }
 
-// Fetch gif on api, render it as ascii and return it
-func InsertGif(id string, url string, rev bool) (imgs []RenderedImg, err error) {
-	g, err := GetFromUrl(url)
-	if err != nil {
-		return imgs, err
-	}
-
-	stmt, err := db.Prepare("INSERT INTO gif (api_id) VALUES (?)")
-	defer stmt.Close()
-	if err != nil {
-		return imgs, err
-	}
-	_, err = stmt.Exec(id)
-	if err != nil {
-		return imgs, err
-	}
-
-	imgs = renderGif(g)
-	for i, srcImg := range imgs {
-		stmt, err := db.Prepare("INSERT INTO gif_data (frame_nb, delay, frame, gif_id) VALUES (?, ?, ?, ?)")
-		defer stmt.Close()
-		if err != nil {
-			return imgs, err
-		}
-		_, err = stmt.Exec(i, srcImg.Delay, srcImg.Output, id)
-		if err != nil {
-			return imgs, err
-		}
-	}
-	// Reverse gif
-	if rev {
-		for i := len(imgs)/2 - 1; i >= 0; i-- {
-			opp := len(imgs) - 1 - i
-			imgs[i], imgs[opp] = imgs[opp], imgs[i]
-		}
-	}
-	return imgs, nil
-}
-
-func GetPreview(url string) (res string) {
-	// set image scale factor for ANSIPixel grid, background color and scale mode
-	tx, ty := 30, 9
-	sfy, sfx := ansimage.BlockSizeY, ansimage.BlockSizeX
-	mc := color.RGBA{0x00, 0x00, 0x00, 0xff}
-	dm := ansimage.DitheringMode(0)
-	sm := ansimage.ScaleMode(2)
-	pix, _ := ansimage.NewScaledFromURL(url, sfy*ty, sfx*tx, mc, sm, dm)
-	pix.SetMaxProcs(runtime.NumCPU())
-	res = pix.Render()
-	return
-}
-
-// Split gif, transform to ansi code and return a slice of images:delay
-func renderGif(g *gif.GIF) (imgs []RenderedImg) {
+func (g *AnsiGif) Render() {
 	// https://stackoverflow.com/a/33296596/8135079
 	defer func() {
 		if r := recover(); r != nil {
@@ -93,10 +44,10 @@ func renderGif(g *gif.GIF) (imgs []RenderedImg) {
 		}
 	}()
 
-	imgWidth, imgHeight := maxDimensions(g)
+	imgWidth, imgHeight := g.maxDimensions()
 
 	overpaintImage := image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
-	draw.Draw(overpaintImage, overpaintImage.Bounds(), g.Image[0], image.ZP, draw.Src)
+	draw.Draw(overpaintImage, overpaintImage.Bounds(), g.Gif.Image[0], image.ZP, draw.Src)
 
 	// set image scale factor for ANSIPixel grid, background color and scale mode
 	tx, ty := 30, 9
@@ -105,23 +56,22 @@ func renderGif(g *gif.GIF) (imgs []RenderedImg) {
 	dm := ansimage.DitheringMode(0)
 	sm := ansimage.ScaleMode(2)
 
-	for i, srcImg := range g.Image {
+	for i, srcImg := range g.Gif.Image {
 		draw.Draw(overpaintImage, overpaintImage.Bounds(), srcImg, image.ZP, draw.Over)
 		pix, _ := ansimage.NewScaledFromImage(overpaintImage, sfy*ty, sfx*tx, mc, sm, dm)
 		pix.SetMaxProcs(runtime.NumCPU())
-		imgs = append(imgs, RenderedImg{Delay: g.Delay[i], Output: pix.Render()})
+		g.Rendered = append(g.Rendered, RenderedImg{Delay: g.Gif.Delay[i], Output: pix.Render()})
 	}
-	return imgs
 }
 
-// Get max Gif dimensions.
-func maxDimensions(gif *gif.GIF) (x, y int) {
+// Get max AnsiGif dimensions.
+func (g *AnsiGif) maxDimensions() (x, y int) {
 	var lowestX int
 	var lowestY int
 	var highestX int
 	var highestY int
 
-	for _, img := range gif.Image {
+	for _, img := range g.Gif.Image {
 		if img.Rect.Min.X < lowestX {
 			lowestX = img.Rect.Min.X
 		}
@@ -140,17 +90,74 @@ func maxDimensions(gif *gif.GIF) (x, y int) {
 }
 
 // If anything bad happen, be cute
-func OopsGif() []RenderedImg {
+func (g *AnsiGif) Oops() {
 	oopsFile, err := os.Open("static/img/oops.gif")
 	defer oopsFile.Close()
 	if err != nil {
 		// Something is really wrong, just stop everything
 		panic("can't even open rescue gif")
 	}
-	g, err := gif.DecodeAll(oopsFile)
+	g.Gif, err = gif.DecodeAll(oopsFile)
 	if err != nil {
 		// Something is really wrong, just stop everything
 		panic("can't even decode rescue gif")
 	}
-	return renderGif(g)
+}
+
+func (g *AnsiGif) Insert(id string) (err error) {
+	stmt, err := db.Prepare("INSERT INTO gif (api_id) VALUES (?)")
+	defer stmt.Close()
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
+	for i, srcImg := range g.Rendered {
+		stmt, err := db.Prepare("INSERT INTO gif_data (frame_nb, delay, frame, gif_id) VALUES (?, ?, ?, ?)")
+		defer stmt.Close()
+		if err != nil {
+			return err
+		}
+		_, err = stmt.Exec(i, srcImg.Delay, srcImg.Output, id)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (g *AnsiGif) Reverse() {
+	for i := len(g.Rendered)/2 - 1; i >= 0; i-- {
+		opp := len(g.Rendered) - 1 - i
+		g.Rendered[i], g.Rendered[opp] = g.Rendered[opp], g.Rendered[i]
+	}
+}
+
+func (g *AnsiGif) Preview() (res string) {
+	// set image scale factor for ANSIPixel grid, background color and scale mode
+	tx, ty := 30, 9
+	sfy, sfx := ansimage.BlockSizeY, ansimage.BlockSizeX
+	mc := color.RGBA{0x00, 0x00, 0x00, 0xff}
+	dm := ansimage.DitheringMode(0)
+	sm := ansimage.ScaleMode(2)
+	pix, _ := ansimage.NewScaledFromImage(g.Gif.Image[0], sfy*ty, sfx*tx, mc, sm, dm)
+	pix.SetMaxProcs(runtime.NumCPU())
+	res = pix.Render()
+	return
+}
+
+func GetPreview(url string) (res string) {
+	// set image scale factor for ANSIPixel grid, background color and scale mode
+	tx, ty := 30, 9
+	sfy, sfx := ansimage.BlockSizeY, ansimage.BlockSizeX
+	mc := color.RGBA{0x00, 0x00, 0x00, 0xff}
+	dm := ansimage.DitheringMode(0)
+	sm := ansimage.ScaleMode(2)
+	pix, _ := ansimage.NewScaledFromURL(url, sfy*ty, sfx*tx, mc, sm, dm)
+	pix.SetMaxProcs(runtime.NumCPU())
+	res = pix.Render()
+	return
 }
